f794efdfd2de6218813121cc99f9886ba18c9fe0
Yun-hanseo <gogodoom59@gmail.com>
2025-12-12 13:17:54 +0900

    header 부분 구현

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <router-view />
+  <div>
+    <Header v-if="isLoggedIn" />
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
+import Header from "./components/common/Header.vue";
+import { useAuth } from "./composables/useAuth.js";
+
+const { isLoggedIn } = useAuth();
 </script>
